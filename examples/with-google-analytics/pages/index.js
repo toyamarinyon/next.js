608a66d85ca998fcc1b65a0d01886478f4cb07bb
toyama satoshi <toyamarinyon@gmail.com>
2021-01-15 06:48:04 +0900

    Build code!

--- examples/with-google-analytics/pages/index.js
+++ examples/with-google-analytics/pages/index.js
@@ -3,7 +3,7 @@ import Page from '../components/Page'
 export default function Home() {
   return (
     <Page>
-      <h1>This is the Home page</h1>
+      <h1>This is the Home page!</h1>
     </Page>
   )
 }
