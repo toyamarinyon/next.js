aa377e6532ecfd5101eb5cec8017f77d5cd8c679
toyama satoshi <toyamarinyon@gmail.com>
2021-01-16 08:13:36 +0900

    Add sample

--- examples/basic-css/pages/index.js
+++ examples/basic-css/pages/index.js
@@ -2,8 +2,52 @@ import styles from '../styles.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
+    <div className={styles.container}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+        vestibulum, ante eget luctus porta, sapien lectus lacinia purus, quis
+        faucibus turpis magna vitae elit. Nulla fermentum massa at tellus
+        mattis, quis laoreet erat interdum. Aenean a neque quis justo pharetra
+        iaculis at at metus. Aliquam tristique ex id augue vestibulum, quis
+        ullamcorper enim dignissim. Nam malesuada lacus sit amet massa
+        porttitor, ut convallis orci convallis. Cras blandit risus at quam
+        dapibus, eu tincidunt arcu porttitor. Pellentesque tempor fringilla
+        tincidunt. Curabitur ut gravida dui, sed ornare neque. Fusce ante arcu,
+        varius ut nunc et, vestibulum pharetra sapien. Proin ac sapien
+        fringilla, commodo enim non, tempus felis. Aliquam vel urna et magna
+        congue rutrum. Cras vestibulum elementum feugiat. Etiam vel vulputate
+        magna. Fusce vitae urna leo. Curabitur massa metus, accumsan at euismod
+        nec, porttitor in purus. Cras mollis fermentum feugiat. Integer vitae
+        nibh massa. In leo enim, gravida eu pulvinar eget, auctor vitae justo.
+        Aliquam eu ante ullamcorper orci sollicitudin semper. Donec tortor erat,
+        malesuada nec magna quis, dictum faucibus dui. Suspendisse tincidunt
+        tellus ac ullamcorper blandit. Sed dui lectus, hendrerit eu consequat
+        in, imperdiet id dolor. Nullam eu ligula eros. Quisque non nunc vitae
+        felis elementum fermentum. Integer nulla metus, fermentum sit amet
+        sagittis finibus, suscipit vel tortor. Sed sed pretium nisl, at tempor
+        erat. Pellentesque condimentum hendrerit leo, at ullamcorper orci
+        facilisis vel. Vestibulum non tellus eget dolor fringilla suscipit. Nam
+        metus ante, molestie vel dapibus vitae, convallis ac nunc. Donec
+        eleifend risus sed erat ornare, nec rhoncus nisl ullamcorper. Nam
+        aliquet, lorem eu cursus convallis, ligula nunc pretium erat, sed
+        vestibulum lectus quam ut felis. Cras sit amet turpis quis erat
+        efficitur elementum. Sed varius mauris non dolor tempus vehicula. Donec
+        condimentum ut odio sed rutrum. Aliquam vitae bibendum felis. Mauris
+        tempus purus urna, nec feugiat massa pulvinar vitae. Vestibulum a
+        porttitor urna, vitae tempor metus. Donec et ipsum maximus, tincidunt
+        risus quis, scelerisque dui. Nam vel est mattis, suscipit nibh vitae,
+        pharetra quam. Integer nec pellentesque nunc. Morbi et ex eu velit
+        convallis blandit. Maecenas ornare eget sapien quis interdum. Nam
+        porttitor ornare cursus. Cras neque velit, vulputate sed convallis nec,
+        egestas id ipsum. Nam eu dui bibendum, tempor eros nec, gravida nunc.
+        Vivamus nunc diam, consectetur sed facilisis non, finibus a eros.
+        Suspendisse potenti. Maecenas porttitor erat libero, a aliquet ligula
+        viverra vitae. Aliquam sit amet diam tempus, lacinia magna at, malesuada
+        tellus. Nunc eu congue massa. Phasellus quis suscipit diam, et congue
+        orci. Donec pretium massa quis ipsum vehicula, sed scelerisque eros
+        ultrices. Cras congue leo eu nulla molestie, sit amet tempor justo
+        faucibus.
+      </p>
     </div>
   )
 }
